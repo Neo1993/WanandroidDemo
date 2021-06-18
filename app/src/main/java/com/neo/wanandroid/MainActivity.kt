e93eb90db95c7762f94a519007878b4e82e4845c
Neo <melijun@163.com>
2021-06-18 19:39:28 +0800

    底部控件初始化

--- app/src/main/java/com/neo/wanandroid/MainActivity.kt
+++ app/src/main/java/com/neo/wanandroid/MainActivity.kt
@@ -2,12 +2,26 @@ package com.neo.wanandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
     }
 
+    private fun initView(){
+
+    }
+
+
+    val mOnNavigationItemSelectedListener : BottomNavigationView.OnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener{
+        when (it.itemId) {
+            R.id.home -> ""
+
+        }
+        true
+    }
 
 }
